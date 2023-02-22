f6557d3d3d9c2e2a10aeef3c180e868ea896a135
Charlz <murtuza.bori24@gmail.com>
2023-02-22 21:09:36 +0530

    Added history for image and text prompts

--- business/models.py
+++ business/models.py
@@ -11,13 +11,11 @@ import os
 # 1) The UserProfile Module
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    phone_number = models.CharField(max_length=20, blank=True)
-    phone_ID = models.CharField(max_length=40, blank=True)
-
-    # Utility Variables
+    phone_number = models.CharField(max_length=20, blank=False)
+    phone_ID = models.CharField(max_length=40, blank=False)
     uniqueID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    date_created = models.DateTimeField(auto_now_add=True, blank=True)
-    last_updated = models.DateTimeField(auto_now=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    last_updated = models.DateTimeField(auto_now=True, null=True)
 
     def save(self, *args, **kwargs):
         if self.date_created is None:
@@ -29,15 +27,14 @@ class Subscription(models.Model):
     profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     subscription_type = models.CharField(max_length=20, blank=True, default="Free")
     subscription_status = models.CharField(max_length=20, blank=True, default="Active")
-    subscription_start_date = models.DateTimeField(auto_now_add=True, blank=True)
-
+    subscription_start_date = models.DateTimeField(auto_now_add=True, null=True)
+    subscription_end_date = models.DateTimeField(blank=True, null=True)
+    history_text_prompt = models.TextField(blank=True)
+    history_image_prompt = models.TextField(blank=True)
     free_prompt_count = models.IntegerField(default=0)
-    # total_prompt_count = models.IntegerField(default=0)
-
-    # Utility Variables
+    free_image_count = models.IntegerField(default=0)
     uniqueID = models.UUIDField(default=uuid4, editable=False)
-    subscription_start_date = models.DateTimeField(null=True, blank=True)
-    last_updated = models.DateTimeField(auto_now=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True, null=True)
 
     def save(self, *args, **kwargs):
         if self.subscription_start_date is None and self.subscription_type != "Free":
