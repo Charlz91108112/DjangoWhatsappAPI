6b2b12979fb0082359c239d64799d7a46560efe2
Charlz <murtuza.bori24@gmail.com>
2023-06-26 11:56:03 +0530

    Removed Credentials

--- business/urls.py
+++ business/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
 path('',views.home, name='home'),
-path('f2bfdb2c-654c-49e8-a335-14d71798a793',views.WhatsappWebhook, name = 'whatsapp-webhook'),
+path('webhook-code',views.WhatsappWebhook, name = 'whatsapp-webhook'),
 ]
